633b80fa11e0e582409c07128204f779b2f763f8
Zhenghao Lou <rhzx3519@gmail.com>
2023-07-06 16:52:27 +0800

    feat: typescript 入门

--- src/screens/utils/index.ts
+++ src/screens/utils/index.ts
@@ -1,27 +1,29 @@
 import { useEffect, useState } from "react";
 
-export const isFalsy = (value) => (value === 0 ? false : !value);
+export const isFalsy = (value: unknown) => (value === 0 ? false : !value);
 
-export const cleanObject = (object) => {
+export const cleanObject = (object: object) => {
   const result = { ...object };
   Object.keys(result).forEach((key) => {
     // 0会被delete
+    // @ts-ignore
     const value = result[key];
     if (isFalsy(value)) {
+      // @ts-ignore
       delete result[key];
     }
   });
   return result;
 };
 
-export const useMount = (callback) => {
+export const useMount = (callback: () => void) => {
   useEffect(() => {
     callback();
   }, []);
 };
 
 // 延迟更新value
-export const useDebounce = (value, delay) => {
+export const useDebounce = <T>(value: T, delay: number = 1000): T => {
   const [debouncedValue, setDebouncedValue] = useState(value);
   useEffect(() => {
     const timeout = setTimeout(() => setDebouncedValue(value), delay);
