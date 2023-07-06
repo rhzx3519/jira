6107b07e8bcb4403436a6c1342fb708713e42f93
Zhenghao Lou <rhzx3519@gmail.com>
2023-07-06 15:29:06 +0800

    add useMount, useDebounce custom hooks

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -2,7 +2,7 @@ import { SearchPanel } from "./search-panel";
 import { List } from "./list";
 import { useEffect, useState } from "react";
 import * as qs from "qs";
-import { cleanObject } from "../utils";
+import { cleanObject, useDebounce, useMount } from "../utils";
 
 const apiUrl = process.env.REACT_APP_JIRA_API_URL;
 
@@ -11,27 +11,28 @@ export const ProjectListScreen = () => {
     name: "",
     personId: "",
   });
+  const debouncedParam = useDebounce(param, 2000);
 
   const [list, setList] = useState([]);
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
-    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(
-      async (response) => {
-        if (response.ok) {
-          setList(await response.json());
-        }
-      },
-    );
-  }, [param]);
+    fetch(
+      `${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`,
+    ).then(async (response) => {
+      if (response.ok) {
+        setList(await response.json());
+      }
+    });
+  }, [debouncedParam]);
 
-  useEffect(() => {
+  useMount(() => {
     fetch(`${apiUrl}/users`).then(async (response) => {
       if (response.ok) {
         setUsers(await response.json());
       }
     });
-  }, []);
+  });
 
   return (
     <div>
