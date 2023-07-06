87394445fe233db2cc7be4b4c48cfe1fcca49874
Zhenghao Lou <rhzx3519@gmail.com>
2023-07-06 21:37:19 +0800

    feat: login register

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,15 @@ import logo from "./logo.svg";
 import "./App.css";
 import { ProjectListScreen } from "./screens/project-list";
 import { TsReactTest } from "./try-use-array";
-import { LoginScreen } from "./screens/login";
+import { useAuth } from "./context/auth-context";
+import { AuthenticatedApp } from "./authenticated-app";
+import { UnauthenticatedApp } from "./unauthenticated-app";
 
 function App() {
+  const { user } = useAuth();
   return (
     <div className="App">
-      {/*<TsReactTest />*/}
-      {/*<ProjectListScreen />*/}
-      <LoginScreen />
+      {user ? <AuthenticatedApp /> : <UnauthenticatedApp />}
     </div>
   );
 }
