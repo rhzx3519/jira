224f6a79df5e1d5f2f1fbab8ab45ec4d3f61fa54
Zhenghao Lou <rhzx3519@gmail.com>
2023-07-07 18:02:12 +0800

    feat: 项目列表页布局调整

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { ProjectListScreen } from "./screens/project-list";
 import { useAuth } from "./context/auth-context";
-import { Button } from "antd";
+import { Button, Dropdown, Menu } from "antd";
 import styled from "@emotion/styled";
 import { Row } from "./components/lib";
+import { ReactComponent as SoftwareLogo } from "assets/logo.svg";
 
 /**
  * grid 和 flex 各自的应用场景
@@ -17,18 +18,29 @@ import { Row } from "./components/lib";
  * @constructor
  */
 export function AuthenticatedApp() {
-  const { logout } = useAuth();
+  const { logout, user } = useAuth();
 
   return (
     <Container>
       <Header>
         <HeaderLeft gap={true}>
+          <SoftwareLogo width={"8rem"} color={"rgb(38, 132, 255)"} />
           <h2>Logo</h2>
           <h2>项目</h2>
           <h2>用户</h2>
         </HeaderLeft>
         <HeaderRight>
-          <Button onClick={() => logout()}>登出</Button>
+          <Dropdown
+            overlay={
+              <Menu>
+                <Menu.Item key={"logout"}>
+                  <a onClick={logout}>登出</a>
+                </Menu.Item>
+              </Menu>
+            }
+          >
+            <a onClick={(e) => e.preventDefault()}>Hi, {user?.name}</a>
+          </Dropdown>
         </HeaderRight>
       </Header>
       <Main>
@@ -38,40 +50,26 @@ export function AuthenticatedApp() {
   );
 }
 
+const Header = styled(Row)`
+  padding: 3.2rem;
+  box-shadow: 0 0 5px 0 rgba(0, 0, 0, 0.1);
+  z-index: 1;
+`;
+
 const HeaderItem = styled.h3`
   margin-right: 3rem;
 `;
 
-const HeaderLeft = styled(Row)``;
+const HeaderLeft = styled(Row)`
+  padding: 3.2rem;
+`;
+
 const HeaderRight = styled.div``;
 
 const Container = styled.div`
   display: grid;
-  grid-template-rows: 6rem 1fr 6rem;
-  grid-template-columns: 20rem 1fr 20rem;
-  grid-template-areas:
-    "header header header"
-    "nav main aside"
-    "footer footer footer";
+  grid-template-rows: 6rem 1fr;
   height: 100vh;
 `;
 
-const Header = styled.header`
-  grid-area: header;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
-`;
-const Main = styled.header`
-  grid-area: main;
-`;
-const Nav = styled.header`
-  grid-area: nav;
-`;
-const Aside = styled.header`
-  grid-area: aside;
-`;
-const Footer = styled.header`
-  grid-area: footer;
-`;
+const Main = styled.main``;
--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -5,6 +5,7 @@ import * as qs from "qs";
 import { cleanObject, useDebounce, useMount } from "../utils";
 import { apiUrl } from "../const";
 import { useHttp } from "../utils/http";
+import styled from "@emotion/styled";
 
 export const ProjectListScreen = () => {
   const [param, setParam] = useState({
@@ -25,9 +26,14 @@ export const ProjectListScreen = () => {
   });
 
   return (
-    <div>
+    <Container>
+      <h1>项目列表</h1>
       <SearchPanel users={users} param={param} setParam={setParam} />
       <List users={users} list={list} />
-    </div>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  padding: 3.2rem;
+`;
